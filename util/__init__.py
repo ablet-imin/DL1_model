508358d66033065362f0d8d3d34c564f84cd0642
ablet-imin <ablet.imin@gmail.com>
2021-06-15 21:05:41 +0200

    update

--- util/__init__.py
+++ util/__init__.py
@@ -1,5 +1,6 @@
 
 from util.tools import *
-from util.eff_tools import momentum_space, efficiency_hist, efficiecy_mean_std
+from util.eff_tools import momentum_space, efficiency_hist, efficiecy_mean_std, efficiecy_from_mean
 
-__all__ = ["DL1_score", "plot_prob_score", "plot_prob_score_from_model", "get_mean_score", "momentum_space", "efficiency_hist", "efficiecy_mean_std"]
+__all__ = ["DL1_score", "plot_prob_score", "plot_prob_score_from_model", "get_mean_score", "momentum_space", "efficiency_hist",
+ "efficiecy_mean_std", "efficiecy_from_mean"]
